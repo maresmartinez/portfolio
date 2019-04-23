0320366d3e9a9c59779521173b89e8ff3c143467
Mariel Martinez <mcortez.martinez@gmail.com>
2019-04-23 18:32:02 -0400

    Created homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ContactComponent
+    ContactComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
